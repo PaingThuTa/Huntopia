437e2c58bd64243f3a591216e79b04ae6966c8ea
PaingThuTa <paingthuta472@gmail.com>
2026-02-11 15:34:11 +0700

    Adjust achievement header styling

--- app/src/main/java/com/example/huntopia/AchievementItem.kt
+++ app/src/main/java/com/example/huntopia/AchievementItem.kt
@@ -3,5 +3,6 @@ package com.example.huntopia
 data class AchievementItem(
     val title: String,
     val achievedDate: String,
-    val achievedTime: String
+    val achievedTime: String,
+    val imageName: String
 )
